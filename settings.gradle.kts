3066100d81345baa987e10a77221a32e5561fa80
raquezha <raquezha@gmail.com>
2021-05-16 02:45:40 +0800

    Let's go to the cloud

---
+++ settings.gradle.kts
@@ -0,0 +1,1 @@
+rootProject.name = "heograpiya-server"
---
+++ src/Application.kt
@@ -0,0 +1,78 @@
+package com.raquezha
+
+import io.ktor.application.*
+import io.ktor.auth.*
+import io.ktor.features.*
+import io.ktor.html.*
+import io.ktor.http.*
+import io.ktor.response.*
+import io.ktor.routing.*
+import io.ktor.sessions.*
+import kotlinx.html.body
+import kotlinx.html.h1
+import kotlinx.html.li
+import kotlinx.html.ul
+
+fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
+
+@Suppress("unused") // Referenced in application.conf
+@kotlin.jvm.JvmOverloads
+fun Application.module(testing: Boolean = false) {
+    install(Authentication) {
+        basic("myBasicAuth") {
+            realm = "Ktor Server"
+            validate {
+                if (it.name == "test" && it.password == "password") {
+                    UserIdPrincipal(it.name)
+                } else {
+                    null
+                }
+            }
+        }
+    }
+
+    install(Sessions) {
+        cookie<MySession>("MY_SESSION") {
+            cookie.extensions["SameSite"] = "lax"
+        }
+    }
+
+    install(ContentNegotiation) {
+
+    }
+
+    routing {
+        get("/") {
+            call.respondText("HELLO WORLD! Fuckers!", contentType = ContentType.Text.Plain)
+        }
+
+        get("/html-dsl") {
+            call.respondHtml {
+                body {
+                    h1 { +"HTML" }
+                    ul {
+                        for (n in 1..10) {
+                            li { +"$n" }
+                        }
+                    }
+                }
+            }
+        }
+
+        authenticate("myBasicAuth") {
+            get("/protected/route/basic") {
+                val principal = call.principal<UserIdPrincipal>()!!
+                call.respondText("Hello ${principal.name}")
+            }
+        }
+
+        get("/session/increment") {
+            val session = call.sessions.get<MySession>() ?: MySession()
+            call.sessions.set(session.copy(count = session.count + 1))
+            call.respondText("Counter is ${session.count}. Refresh to increment.")
+        }
+    }
+}
+
+data class MySession(val count: Int = 0)
+
